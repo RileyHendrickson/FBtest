672097b60f0ef8de629affb6c27039bb282a3718
Riley Hendrickson <Riley@Rileys-iMac.local>
2015-01-31 10:47:53 -0800

    Added video player via GIF code - Included a gif in project for testing - Commented out code that we will be overriding.

--- SportsBetting/ViewController.swift
+++ SportsBetting/ViewController.swift
@@ -12,6 +12,9 @@ class ViewController: UIViewController, FBLoginViewDelegate {
     
     @IBOutlet var fbLoginView : FBLoginView!
     
+    //@IBOutlet var profilePictureView : FBProfilePictureView! - This is the IBOutlet for the picture view
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,12 +22,55 @@ class ViewController: UIViewController, FBLoginViewDelegate {
         self.fbLoginView.delegate = self
         self.fbLoginView.readPermissions = ["public_profile", "email", "user_friends", "user_photos"]
         
+        
+        
+        //Added video file for launch screen background
+        var filePath = NSBundle.mainBundle().pathForResource("tumblr", ofType: "gif")
+        var gif = NSData(contentsOfFile: filePath!)
+        
+        var webViewBG = UIWebView(frame: self.view.frame)
+        webViewBG.loadData(gif, MIMEType: "image/gif", textEncodingName: nil, baseURL: nil)
+        webViewBG.userInteractionEnabled = false;
+        self.view.addSubview(webViewBG)
+        
+        var filter = UIView()
+        filter.frame = self.view.frame
+        filter.backgroundColor = UIColor.blackColor()
+        filter.alpha = 0.05
+        self.view.addSubview(filter)
+        
+        var welcomeLabel = UILabel(frame: CGRectMake(0, 100, self.view.bounds.size.width, 100))
+        welcomeLabel.text = "WELCOME"
+        welcomeLabel.textColor = UIColor.whiteColor()
+        welcomeLabel.font = UIFont.systemFontOfSize(50)
+        welcomeLabel.textAlignment = NSTextAlignment.Center
+        self.view.addSubview(welcomeLabel)
+        
+        //var loginBtn = UIButton(frame: CGRectMake(40, 360, 240, 40))
+        //loginBtn.layer.borderColor = UIColor.whiteColor().CGColor
+        //loginBtn.layer.borderWidth = 2
+        //loginBtn.titleLabel!.font = UIFont.systemFontOfSize(24)
+        //loginBtn.tintColor = UIColor.whiteColor()
+        //loginBtn.setTitle("Login", forState: UIControlState.Normal)
+        //self.view.addSubview(loginBtn)
+        
+        //var signUpBtn = UIButton(frame: CGRectMake(40, 420, 240, 40))
+        //signUpBtn.layer.borderColor = UIColor.whiteColor().CGColor
+        //signUpBtn.layer.borderWidth = 2
+        //signUpBtn.titleLabel!.font = UIFont.systemFontOfSize(24)
+        //signUpBtn.tintColor = UIColor.whiteColor()
+        //signUpBtn.setTitle("Sign Up", forState: UIControlState.Normal)  -   [Need to replace with the FB login button]
+        //self.view.addSubview(signUpBtn)
+        
+        
     }
     
     // Facebook Delegate Methods
     
     func loginViewShowingLoggedInUser(loginView : FBLoginView!) {
         println("User Logged In")
+        
+        
     }
     
     func loginViewFetchedUserInfo(loginView : FBLoginView!, user: FBGraphUser) {
@@ -34,10 +80,22 @@ class ViewController: UIViewController, FBLoginViewDelegate {
         var userEmail = user.objectForKey("email") as String
         println("User Email: \(userEmail)")
         
+        var profileImageView = FBProfilePictureView()
+        
         // Get List Of Friends From FB
         var friendsRequest : FBRequest = FBRequest.requestForMyFriends()
         friendsRequest.startWithCompletionHandler{(connection:FBRequestConnection!, result:AnyObject!, error:NSError!) -> Void in
             var resultdict = result as NSDictionary
+            
+            
+            //self.profilePictureView.profileID = user.objectID                                           Grabbing the photo from the FB profile
+            //self.profilePictureView.pictureCropping = FBProfilePictureCropping.Square                   Setting the photo to a square
+            //self.profilePictureView.frame = CGRectMake(200, 200, 100, 100)                              Setting the Photo frame
+            //self.profilePictureView.layer.cornerRadius = self.profilePictureView.frame.size.width / 2   Setting the photo size
+            
+            
+            
+            
             println("Result Dict: \(resultdict)")
             var data : NSArray = resultdict.objectForKey("data") as NSArray
             
@@ -50,10 +108,13 @@ class ViewController: UIViewController, FBLoginViewDelegate {
             var friends = resultdict.objectForKey("data") as NSArray
             println("Found \(friends.count) friends")
             
-            //Future Item - Abilty to invite friends in return for users who sign up allow a cach bonous
-            //To be paid out to those users in a disclosed sum
-
+           
+            
         }
+        
+            //Added a view for
+            //self.view.addSubview(profilePictureView)
+        
         
     }
     
@@ -65,18 +126,17 @@ class ViewController: UIViewController, FBLoginViewDelegate {
         println("Error: \(handleError.localizedDescription)")
     }
     
-    // Get List Of Friends
-    
-    //Add video files below
-    
-    
-    
-    
     
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+
+    override func preferredStatusBarStyle() -> UIStatusBarStyle {
+        return UIStatusBarStyle.LightContent
+    }
+    
     
 }
