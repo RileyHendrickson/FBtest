8578c5ecade8e85a5538aacd1197e2ac49478186
Riley Hendrickson <Riley@Rileys-iMac.local>
2015-01-30 21:38:38 -0800

    commit

---
+++ SportsBetting/Bridge-Header.h
@@ -0,0 +1,13 @@
+//
+//  Bridge-Header.h
+//  SportsBetting
+//
+//  Created by Riley Hendrickson on 1/30/15.
+//  Copyright (c) 2015 Riley Hendrickson. All rights reserved.
+//
+
+#ifndef SportsBetting_Bridge_Header_h
+#define SportsBetting_Bridge_Header_h
+#import <FacebookSDK/FacebookSDK.h>
+
+#endif
